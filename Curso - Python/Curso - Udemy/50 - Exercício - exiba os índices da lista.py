2994db9da72a1150c37d4d4e3cd8e1014949a116
JhnV21 <vitor.1943@gmail.com>
2022-12-28 11:56:27 -0300

    28/12

---
+++ Curso - Python/Curso - Udemy/50 - Exercício - exiba os índices da lista.py
@@ -0,0 +1,11 @@
+"""
+Exercício
+Exiba os índices da lista
+"""
+lista = ['Maria', 'Helena', 'Luiz']
+lista.append('João')
+numero = 0 
+
+for nome in lista:
+    print(numero, nome)
+    numero += 1
